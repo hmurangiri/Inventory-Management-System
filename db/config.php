0cef34b6e99fbdbe4049abe4c4d8c6b25676f324
Henry Murangiri <37686400+hmurangiri@users.noreply.github.com>
2024-07-03 12:10:41 +0300

    Add files via upload

--- db/config.php
+++ db/config.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "localhost:3306"; // Your server name
+$username = "skillswi_invent"; // Your database username
+$password = "Testuser2023"; // Your database password
+$dbname = "skillswi_inventory2"; // Your database name
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
